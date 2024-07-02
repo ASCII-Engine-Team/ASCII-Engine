929f01f8c4567f84d3b954449b5bd937669c7d9d
Bradley <128531544+Drkud@users.noreply.github.com>
2024-07-02 03:25:54 +0000

    it clears the consle

--- Constants.java
+++ Constants.java
@@ -1,4 +1,5 @@
 public class Constants {
 	public static final int SCREEN_WIDTH = 192;
 	public static final int SCREEN_HEIGHT = 48;
+	public static final int FPS = 33;
 }
--- Test.java
+++ Test.java
@@ -3,22 +3,33 @@ import java.util.concurrent.TimeUnit;
 class Test {
 	static void fillScreen(char character) {
 		String screen = "";
-		for (int i = 0; i < Constants.SCREEN_WIDTH * Constants.SCREEN_HEIGHT; i++) {
+		for (int i = 0; i < Constants.SCREEN_HEIGHT; i++) {
+			// so it works on nonchrome books
+			for (int j = 0; j < Constants.SCREEN_WIDTH; j++) {
 			screen += character;
+			}
+			screen += "\n";
 		}
 		
 		// System.out.print("\033[H");
 		// System.out.print("\033[2J");
 		System.out.print(screen);
 	}
+	public static void clearScreen() {  
+		System.out.print("\033[2J");  
+		System.out.flush();  
+	}  
 
 	public static void main(String[] args) throws InterruptedException{
 		final int millisecondPause = Integer.parseInt(args[0]);
+		
 		while (true) {
 			fillScreen('"');
 			TimeUnit.MILLISECONDS.sleep(millisecondPause);
+			clearScreen();
 			fillScreen('#');
 			TimeUnit.MILLISECONDS.sleep(millisecondPause);
+			clearScreen();
 		}
 	}
 }
