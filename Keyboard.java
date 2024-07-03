8e67a0ac2ad7ae6cad0fda6f72faca86fb6605bb
Elliot Holmes <133877996+eholm62@users.noreply.github.com>
2024-07-03 04:09:06 +0000

    added jnativehook library and deleted test files

--- Keyboard.java
+++ Keyboard.java
@@ -1,42 +1,5 @@
-import java.awt.event.*;
+import com.github.kwhat.jnativehook.keyboard.*;
 
-public class Keyboard implements KeyListener {
-    public static void main(String[] args) {
-        Keyboard keyboard = new Keyboard();
-        while (true) {
-
-        }
-        
-    }
+public class Keyboard implements NativeKeyListener {
     
-    private boolean wPressed = false;
-    private boolean aPressed = false;
-    private boolean sPressed = false;
-    private boolean dPressed = false;
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case 37:
-                System.out.println(37);
-                break;
-            case 38:
-                System.out.println(38);
-                break;
-            case 39:
-                System.out.println(39);
-                break;
-            case 40:
-                System.out.println(40);
-                break;
-        }
-    }
-    
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {}
 }
